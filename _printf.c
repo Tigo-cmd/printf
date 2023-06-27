c689277171255d90ec63483d8cd277e4555eabdb
Tigo-cmd <ja16162040@gmail.com>
2023-06-27 15:38:04 -0400

    updated

--- _printf.c
+++ _printf.c
@@ -2,6 +2,52 @@
 #include <stdarg.h>
 #include <unistd.h>
 /**
+* _charstring - prints char snd string identifiers
+* @str: first parameter
+* @chr: pointer to charcter to be printed
+* @ch: pointer to counter
+*
+* Decsription: function that prints char and string identifiers
+* Return: number of charcters printed
+*/
+int *_charstring(va_list str, char *chr, int *ch)
+{
+	ch = 0;
+	char c;
+	char *s;
+	char per;
+
+	switch (*chr)
+	{
+		case '\0':
+			break;
+			break;
+		case 'c':
+			c = va_arg(str, int);
+			write(1, &c, 1);
+			ch++;
+			break;
+		case 's':
+			s = va_arg(str, char *);
+			for (; *s; s++)
+			{
+				write(1, s, 1);
+				ch++;
+			}
+			break;
+		case '%':
+			per = '%';
+			write(1, &per, 1);
+			ch++;
+			break;
+		default:
+			break;
+	}
+	return (ch);
+
+}
+
+/**
 * _printf - my clone of the printf function
 * @format: the character string. it is to be strictly followed
 *
@@ -12,11 +58,6 @@ int _printf(const char *format, ...)
 {
 	char *chr = (char *)format;
 	int ch = 0;
-	char c;
-	char *s;
-	char per;
-	int i;
-	int d;
 
 	va_list str;
 
@@ -28,42 +69,7 @@ int _printf(const char *format, ...)
 		if (*chr == '%')
 		{
 			chr++;
-			switch (*chr)
-			{
-				case '\0':
-					break;
-					break;
-				case 'c':
-					c = va_arg(str, int);
-					write(1, &c, 1);
-					ch++;
-					break;
-				case 's':
-					s = va_arg(str, char *);
-					for (; *s; s++)
-					{
-						write(1, s, 1);
-						ch++;
-					}
-					break;
-				case '%':
-					per = '%';
-					write(1, &per, 1);
-					ch++;
-					break;
-				case 'i':
-					i = va_arg(str, int);
-					write(1, &i, 1);
-					ch++;
-					break;
-				case 'd':
-					d = va_arg(str, int);
-					write(1, &d, 1);
-					ch++;
-					break;
-				default:
-					break;
-			}
+			_charstring(str, chr, &ch);
 		}
 		else
 		{
